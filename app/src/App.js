f186707b473eaf6cb906bbc001c16418c8501dc6
Ryan <rmthomas1998@gmail.com>
2022-05-21 21:21:35 -0400

    request function done

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,28 @@
+import { Octokit } from "@octokit/rest";
+import { useEffect, useState } from "react";
+
 const App = () => {
-  return <></>;
+  // declare Octokit
+  const octokit = new Octokit();
+  // state where commits object will be
+  const [commits, setCommits] = useState();
+
+  useEffect(() => {
+    // function to get all commits for this repository
+    const getCommits = async () => {
+      const commits = await octokit.request(
+        "GET /repos/rmthomas98/github-commit-tracker/commits"
+      );
+      console.log(commits);
+    };
+    getCommits();
+  }, []);
+
+  return (
+    <div className="wrapper">
+      <div className="container"></div>
+    </div>
+  );
 };
 
 export default App;
