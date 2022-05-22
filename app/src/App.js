9105042864acf62e9c5092a3dd5e5cc3efbd09de
Ryan <rmthomas1998@gmail.com>
2022-05-21 21:56:23 -0400

    done with css

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,6 @@
 import { Octokit } from "@octokit/rest";
 import { useEffect, useState } from "react";
+import { format } from "date-fns";
 
 const App = () => {
   // declare Octokit
@@ -23,6 +24,14 @@ const App = () => {
   return (
     <div className="wrapper">
       <div className="container">
+        <div className="label-container">
+          <p className="label">Committer</p>
+          <p className="label">Date</p>
+          <p className="label">Message</p>
+          <p className="label" style={{ maxWidth: 123, marginRight: 0 }}>
+            Link
+          </p>
+        </div>
         {commits
           ? commits.map((element, index) => {
               const { name, date } = element.commit.author;
@@ -30,8 +39,10 @@ const App = () => {
               const { html_url: htmlUrl } = element;
               return (
                 <div className="commit-container" key={index}>
-                  <p className="desc">{name}</p>
-                  <p className="desc">{date}</p>
+                  <p className="desc name">{name}</p>
+                  <p className="desc date">
+                    {format(new Date(date), "MMMM, dd yyyy h:mm aa")}
+                  </p>
                   <p className="desc message">{message}</p>
                   <a
                     href={htmlUrl}
